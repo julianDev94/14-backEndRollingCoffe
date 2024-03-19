75a069b926ab146d58b299adbaeb95bede028145
JulianAballay <juliancabj1210@gmail.com>
2024-03-19 15:24:14 -0300

    creamos la logica de listar productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,8 +1,18 @@
 import Producto from "../database/model/producto.js";
 
-export const listaProductos = (req, res) => {
-  console.log("desde listar productos");
-  res.send("enviar lista de productos...");
+export const listaProductos = async (req, res) => {
+  try {
+    // console.log("desde listar productos");
+    // res.send("enviar lista de productos...");
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje:
+        "No se pudo completar la operacion. Por favor vuelva a intentarlo en otro momento",
+    });
+  }
 };
 
 export const crearProducto = async (req, res) => {
@@ -15,13 +25,13 @@ export const crearProducto = async (req, res) => {
     await productoNuevo.save();
     //enviar el codigo status 201
     res.status(201).json({
-      mensaje: "El producto fue creado correctamente"
-    })
+      mensaje: "El producto fue creado correctamente",
+    });
     // res.send('desde Crear producto');
   } catch (error) {
     console.log(error);
     res.status(400).json({
-      mensaje: "No se pudo procesar la solicitud de crear producto"
-    })
+      mensaje: "No se pudo procesar la solicitud de crear producto",
+    });
   }
 };
